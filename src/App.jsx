8e6a62cfc83616ca248e0ee0bdddf2ea842176aa
Yair <yairmasterlol@gmail.com>
2023-05-18 21:01:24 -0600

    nice

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,65 @@
 import { useState, useEffect } from "react";
 import "./App.css";
-import CryptoJS from "crypto-js";
 
+import CryptoJS from "crypto-js";
+import { Toaster, toast } from "react-hot-toast";
 function App() {
   const [text, setText] = useState("");
   const [encryptedText, setEncryptedText] = useState("");
   const [decryptedText, setDecryptedText] = useState("");
   const [stepEncrypted, setStepEncrypted] = useState("");
+  const [decryptByStep, setDecryptByStep] = useState("");
+  const [textDecrypt, setTextDecrypt] = useState("");
 
   useEffect(() => {
-    if (encryptedText !== "") {
-      const secretKey = "1";
-      const bytes = CryptoJS.AES.decrypt(encryptedText, secretKey);
-      setDecryptedText(bytes.toString(CryptoJS.enc.Utf8));
-    }
-    stepByStep();
+    stepByStep(encryptedText, setStepEncrypted);
   }, [encryptedText]);
-  const stepByStep = () => {
-    console.log(stepEncrypted.length);
+
+  const stepByStep = (text, changeState) => {
     setStepEncrypted("");
-    for (let i = 0; i < encryptedText.length; i++) {
+    setDecryptByStep("");
+    for (let i = 0; i < text.length; i++) {
       setTimeout(() => {
-        setStepEncrypted((stepEncrypted) => stepEncrypted + encryptedText[i]);
+        changeState((state) => state + text[i]);
       }, i * 30);
     }
   };
 
-  const crypting = () => {
-    console.log(text);
-    navigator.clipboard.writeText("holaaaaa");
+  useEffect(() => {
+    stepByStep(decryptedText, setDecryptByStep);
+  }, [decryptedText]);
 
+  const decrypt = (crypt_text) => {
+    setEncryptedText("");
+    console.log(decryptedText);
+    const secretKey = "1";
+    const bytes = CryptoJS.AES.decrypt(crypt_text, secretKey);
+    try {
+      const finalDecrypt = bytes.toString(CryptoJS.enc.Utf8);
+      finalDecrypt.length === 0
+        ? toast.error("Sin resultados")
+        : setDecryptedText(finalDecrypt);
+    } catch (err) {
+      console.log(err);
+      toast.error("sin resultados");
+    }
+    setText("");
+    setEncryptedText("");
+  };
+
+  const crypting = () => {
     if (text !== "") {
+      setTextDecrypt("");
       const secretKey = "1";
       const plaintext = text;
       // Encrypting the plaintext
       const crypted = CryptoJS.AES.encrypt(plaintext, secretKey).toString();
       // setStepEncrypted(crypted);
       setEncryptedText(crypted);
+      setDecryptByStep("");
+      setDecryptedText("");
     } else {
-      alert("Agrega tu texto");
+      toast.error("Agrega Text");
     }
   };
   const copyCrypting = () => {
@@ -47,6 +68,7 @@ function App() {
 
   return (
     <>
+      <Toaster />
       <div className="text-2xl"></div>
       <div className="mb-5">
         <a
@@ -58,15 +80,35 @@ function App() {
       </div>
       {/* <p className="font-bold">Decrypted text: {decryptedText}</p> */}
       <div className="flex items-center justify-center">
+        <button onClick={() => setText("")}>
+          <svg
+            fill="none"
+            viewBox="0 0 15 15"
+            height="1em"
+            width="1em"
+            className="w-6 h-6 mr-2"
+          >
+            <path
+              fill="currentColor"
+              fillRule="evenodd"
+              d="M.877 7.5a6.623 6.623 0 1113.246 0 6.623 6.623 0 01-13.246 0zM7.5 1.827a5.673 5.673 0 100 11.346 5.673 5.673 0 000-11.346zm2.354 3.32a.5.5 0 010 .707L8.207 7.5l1.647 1.646a.5.5 0 01-.708.708L7.5 8.207 5.854 9.854a.5.5 0 01-.708-.708L6.793 7.5 5.146 5.854a.5.5 0 01.708-.708L7.5 6.793l1.646-1.647a.5.5 0 01.708 0z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </button>
         <input
           type="text"
-          placeholder="type here"
+          placeholder="Encriptar..."
           className="input input-bordered"
+          value={text}
           onChange={(e) => setText(e.target.value)}
         />
         <button
           className="btn btn-lg ml-2 btn-success"
-          disabled={stepEncrypted.length !== encryptedText.length}
+          disabled={
+            stepEncrypted.length !== encryptedText.length ||
+            decryptByStep.length !== decryptedText.length
+          }
           onClick={crypting}
         >
           <svg
@@ -81,9 +123,51 @@ function App() {
         </button>
       </div>
 
-      {/* <p className="mt-2">
-        Texto encriptado: <span className="font-bold">{encryptedText}</span>
-      </p> */}
+      <div className="flex items-center justify-center mt-10">
+        <button onClick={() => setTextDecrypt("")}>
+          <svg
+            fill="none"
+            viewBox="0 0 15 15"
+            height="1em"
+            width="1em"
+            className="w-6 h-6 mr-2"
+          >
+            <path
+              fill="currentColor"
+              fillRule="evenodd"
+              d="M.877 7.5a6.623 6.623 0 1113.246 0 6.623 6.623 0 01-13.246 0zM7.5 1.827a5.673 5.673 0 100 11.346 5.673 5.673 0 000-11.346zm2.354 3.32a.5.5 0 010 .707L8.207 7.5l1.647 1.646a.5.5 0 01-.708.708L7.5 8.207 5.854 9.854a.5.5 0 01-.708-.708L6.793 7.5 5.146 5.854a.5.5 0 01.708-.708L7.5 6.793l1.646-1.647a.5.5 0 01.708 0z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </button>
+        <input
+          type="text"
+          placeholder="Desencriptar..."
+          className="input input-bordered"
+          value={textDecrypt}
+          onChange={(e) => setTextDecrypt(e.target.value)}
+        />
+        <button
+          className="btn btn-lg ml-2 btn-info"
+          disabled={
+            stepEncrypted.length !== encryptedText.length ||
+            (decryptByStep.length !== decryptedText.length &&
+              decryptedText !== "")
+          }
+          // disabled={stepEncrypted.length !== encryptedText.length}
+          onClick={() => decrypt(textDecrypt)}
+        >
+          <svg
+            viewBox="0 0 1024 1024"
+            fill="currentColor"
+            height="1em"
+            width="1em"
+            className="w-10 h-10"
+          >
+            <path d="M688 312v-48c0-4.4-3.6-8-8-8H296c-4.4 0-8 3.6-8 8v48c0 4.4 3.6 8 8 8h384c4.4 0 8-3.6 8-8zm-392 88c-4.4 0-8 3.6-8 8v48c0 4.4 3.6 8 8 8h184c4.4 0 8-3.6 8-8v-48c0-4.4-3.6-8-8-8H296zm144 452H208V148h560v344c0 4.4 3.6 8 8 8h56c4.4 0 8-3.6 8-8V108c0-17.7-14.3-32-32-32H168c-17.7 0-32 14.3-32 32v784c0 17.7 14.3 32 32 32h272c4.4 0 8-3.6 8-8v-56c0-4.4-3.6-8-8-8zm445.7 51.5l-93.3-93.3C814.7 780.7 828 743.9 828 704c0-97.2-78.8-176-176-176s-176 78.8-176 176 78.8 176 176 176c35.8 0 69-10.7 96.8-29l94.7 94.7c1.6 1.6 3.6 2.3 5.6 2.3s4.1-.8 5.6-2.3l31-31a7.9 7.9 0 000-11.2zM652 816c-61.9 0-112-50.1-112-112s50.1-112 112-112 112 50.1 112 112-50.1 112-112 112z" />
+          </svg>
+        </button>
+      </div>
 
       <div className="mockup-code  mx-auto mt-10 flex flex-col flex-s items-start">
         <pre data-prefix="$" className="">
@@ -92,14 +176,12 @@ function App() {
         <pre data-prefix=">" className="text-warning ">
           <code>Generando...</code>
         </pre>
-        <pre data-prefix=">" className="text-success">
-          {/* <code className="">{encryptedText}</code> */}
+        <pre data-prefix=">" className="text-success ">
           <code>{stepEncrypted}</code>
+          <code>{decryptByStep}</code>
         </pre>
       </div>
-      {/* <button className="btn" onClick={stepByStep}>
-        btn
-      </button> */}
+
       <div onClick={copyCrypting}>
         <svg
           className="w-8 h-8 cursor-pointer m-2 active:w-6"
